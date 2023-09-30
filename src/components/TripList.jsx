007fc366b0376417235c9b038e3ac1fbd2c7c994
oyatulloxursanaliyev <xursanaliyevoyatullo5@gmail.com>
2023-09-30 23:46:05 +0500

    cursor

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -5,7 +5,7 @@ function TripList({ trips }) {
     <ul className="max-w-max mx-auto flex gap-x-[70px] mt-20">
         {trips && trips.map((trip) => {
             return (
-                <li className='shadow-2xl shadow-teal-700 px-5 py-5' key={trip.title}>
+                <li className='shadow-2xl shadow-teal-700 px-5 py-5 cursor-pointer' key={trip.title}>
                     <h2 className='text-white text-center'>{trip.title}</h2>
                     <p className='text-yellow-500 text-center'>{trip.price}</p>
                     <p className=' text-center'>{trip.loc}</p>
